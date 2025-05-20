72e92cc6a951f98d156ef24272e1f7d203b757f5
sunilkarky <sunilkarki670@gmail.com>
2025-05-20 13:41:03 +0545

    Implemented interview page UI

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,12 @@
+import Agent from "@/components/Agent";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <Agent userName="you" userId="123" type="generate" />
+    </>
+  );
+};
+
+export default Page;
